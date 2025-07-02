a12ae01308e4073890d6ca35b10089cdfbd921f0
zonik811 <38965704+zonik811@users.noreply.github.com>
2025-07-01 22:53:07 -0500

    Delete EjercicioEjercitoJS directory

--- EjercicioEjercitoJS/main.js
+++
@@ -1,7 +0,0 @@
-import { crear_ejercito } from "./Ejercitos.js";
-import { combatir } from "./Guerra_Logica.js";
-
-const alfa = crear_ejercito('Alfa');
-const bravo = crear_ejercito('Bravo');
-
-combatir(alfa,bravo);
