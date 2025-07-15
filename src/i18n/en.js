a9704d064d499670a30b9d941bc8c2f4dd002fec
zonik811 <38965704+zonik811@users.noreply.github.com>
2025-07-14 23:46:18 -0500

    Add files via upload

---
+++ src/i18n/en.js
@@ -0,0 +1,26 @@
+export default {
+  title: 'Rick and Morty Characters',
+
+  filter: {
+    origin: 'Origin',
+    status: 'Status',
+    search: 'Search'
+  },
+
+  status: {
+    alive: 'Alive',
+    dead: 'Dead',
+    unknown: 'Unknown'
+  },
+
+  card: {
+    name:       'Name',
+    status:     'Status',
+    species:    'Species',
+    gender:     'Gender',
+    origin:     'Origin',
+    location:   'Location',
+    dimension:  'Dimension',
+    unknown:    'Unknown'
+  }
+};
---
+++ src/i18n/es.js
@@ -0,0 +1,26 @@
+export default {
+  title: 'Personajes de Rick y Morty',
+
+  filter: {
+    origin: 'Origen',
+    status: 'Estatus',
+    search: 'Buscar'
+  },
+
+  status: {
+    alive: 'Vivo',
+    dead: 'Muerto',
+    unknown: 'Quien sabe?'
+  },
+
+  card: {
+    name:       'Nombre',
+    status:     'Estado',
+    species:    'Especie',
+    gender:     'Genero',
+    origin:     'Origen',
+    location:   'Ubicacion',
+    dimension:  'Dimension',
+    unknown:    'Desconocida'
+  }
+};
